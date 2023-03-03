91e9baa410e29811d75c5eccc5938ef4e770ab4f
gudmunduro <goh00@protonmail.com>
2023-03-03 09:40:24 +0000

    Fix line not being drawn between correct points

--- src/GlUtils/Utils.cpp
+++ src/GlUtils/Utils.cpp
@@ -9,12 +9,12 @@
 void drawLine(const QPointF& from, const QPointF& to) {
     const auto between = (to - from);
     const auto distance = sqrt(QPointF::dotProduct(between, between));
-    const auto angle = acos(QPointF::dotProduct(to, from) / (sqrt(QPointF::dotProduct(to, to)) * sqrt(QPointF::dotProduct(from, from))));
+    const auto angle = between.x() >= 0 ? atan(between.y() / between.x()) : atan(between.y() / between.x()) - M_PI;
 
     glPushMatrix();
     glTranslated(from.x(), from.y(), 0);
-    glRotated(-qRadiansToDegrees(angle), 0, 0, 1);
-    glScaled(distance, 0.001, 1.0);
+    glRotated(qRadiansToDegrees(angle), 0, 0, 1);
+    glScaled(distance, 0.005, 1.0);
 
     glBegin(GL_TRIANGLE_STRIP);
     glVertex2f(0.0, 0.0);
