1c22a8d1dbff40344fa50d77e0efda24d16631bb
gudmunduro <goh00@protonmail.com>
2023-03-05 19:14:46 +0000

    Limit number of operators that can be created

--- src/Controller/Controller.cpp
+++ src/Controller/Controller.cpp
@@ -1,5 +1,7 @@
 #include "Controller.h"
 
+const int kMaxNumberOfOperators = 8;
+
 std::unique_ptr<Controller> Controller::instance = std::make_unique<Controller>();
 
 Controller::Controller(QObject *parent) : QObject(parent) {
@@ -13,7 +15,11 @@ bool Controller::isConnected(){
     return true;
 }
 
-int Controller::addOperator() {
+std::optional<int> Controller::addOperator() {
+    if (operators_.size() >= kMaxNumberOfOperators) {
+        return std::nullopt;
+    }
+
     int id = *std::min_element(availableOperatorIds_.begin(), availableOperatorIds_.end());
     availableOperatorIds_.erase(id);
 
